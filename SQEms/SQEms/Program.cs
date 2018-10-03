b87106191b1636da1a35e2e7c0e0acd2b638ba17
azalac <zalac.austin@gmail.com>
2018-10-03 14:47:09 -0400

    Added solution

---
+++ SQEms/SQEms/Program.cs
@@ -0,0 +1,17 @@
+﻿using SchedulingUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQEms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UITesting.test_box();
+        }
+    }
+}
---
+++ SQEms/SchedulingUI/Testing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingUI
+{
+    public static class UITesting
+    {
+        public static void test_box()
+        {
+            Console.Write(Convert.ToChar(0x250c));
+        }
+
+    }
+}
