5f6ba3bca18b5b4013092fb4b0f7adb7c9a62c7e
memcallen <memcallen5@gmail.com>
2018-10-15 12:53:58 -0400

    Rushed Commit (incoming class)

--- SQEms/SchedulingUI/Interface.cs
+++ SQEms/SchedulingUI/Interface.cs
@@ -1,252 +1,276 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace SchedulingUI
 {
-	public interface IComponent
-	{
-		int ZIndex {get;}
+    public interface IComponent
+    {
+        int ZIndex { get; }
 
-		int Left { get; set; }
-		int Top { get; set; }
-		int Width { get; set; }
-		int Height { get; set; }
+        int Left { get; set; }
+        int Top { get; set; }
+        int Width { get; set; }
+        int Height { get; set; }
 
-		event EventHandler RequestRedraw;
+        event EventHandler RequestRedraw;
 
-		ConsoleColor Background { get; set; }
-		ConsoleColor Foreground { get; set; }
+        ConsoleColor Background { get; set; }
+        ConsoleColor Foreground { get; set; }
 
-		void Draw(IConsole buffer);
-	}
+        void Draw(IConsole buffer);
+    }
 
-	class Comparator<T, R> : IComparer<T>
-		where R : IComparable
-	{
-		public Func<T, R> KeyExtractor { get; set; }
+    class Comparator<T, R> : IComparer<T>
+        where R : IComparable
+    {
+        public Func<T, R> KeyExtractor { get; set; }
 
-		public Comparator(Func<T, R> KeyExtractor)
-		{
-			this.KeyExtractor = KeyExtractor;
-		}
-		
-		public int Compare (T x, T y)
-		{
-			return KeyExtractor (x).CompareTo (KeyExtractor (y));
-		}
-	}
+        public Comparator(Func<T, R> KeyExtractor)
+        {
+            this.KeyExtractor = KeyExtractor;
+        }
 
-	public abstract class Component : IComponent
-	{
-		public Component()
-		{
-		}
-		
-		public event EventHandler<ComponentEventArgs> ComponentAdded;
+        public int Compare(T x, T y)
+        {
+            return KeyExtractor(x).CompareTo(KeyExtractor(y));
+        }
+    }
 
-		public event EventHandler<ComponentEventArgs> ComponentRemoved;
-		
-		public ConsoleColor Background { get; set; }
-		public ConsoleColor Foreground { get; set; }
+    public abstract class Component : IComponent
+    {
+        public Component()
+        {
+        }
 
-		public event EventHandler RequestRedraw;
+        public event EventHandler<ComponentEventArgs> ComponentAdded;
 
-		public virtual void OnRequestRedraw(object sender, EventArgs args)
-		{
-			RequestRedraw (sender, args);
-		}
+        public event EventHandler<ComponentEventArgs> ComponentRemoved;
 
-		public virtual void OnComponentAdded(object sender, ComponentEventArgs args)
-		{
-			ComponentAdded (sender, args);
-		}
+        public ConsoleColor Background { get; set; }
+        public ConsoleColor Foreground { get; set; }
 
-		public virtual void OnComponentRemoved(object sender, ComponentEventArgs args)
-		{
-			ComponentRemoved (sender, args);
-		}
+        public event EventHandler RequestRedraw;
 
-		public int ZIndex { get; set; }
+        public virtual void OnRequestRedraw(object sender, EventArgs args)
+        {
+            RequestRedraw(sender, args);
+        }
 
-		public int Left { get; set; }
+        public virtual void OnComponentAdded(object sender, ComponentEventArgs args)
+        {
+            ComponentAdded(sender, args);
+        }
 
-		public int Top { get; set; }
+        public virtual void OnComponentRemoved(object sender, ComponentEventArgs args)
+        {
+            ComponentRemoved(sender, args);
+        }
 
-		public int Width { get; set; }
+        public int ZIndex { get; set; }
 
-		public int Height { get; set; }
+        public int Left { get; set; }
 
-		public abstract void Draw(IConsole buffer);
+        public int Top { get; set; }
 
-	}
+        public int Width { get; set; }
 
-	public abstract class Container : Component
-	{
-		public readonly List<IComponent> Components = new List<IComponent> ();
+        public int Height { get; set; }
 
-		private Comparator<IComponent, int> compare = new Comparator<IComponent, int> (c => c.ZIndex);
+        public abstract void Draw(IConsole buffer);
 
-		public int Count {
-			get { return Components.Count; }
-		}
+    }
 
-		public virtual void Add(IComponent component)
-		{
-			Components.Add (component);
-			Components.Sort (compare);
+    public abstract class Container : Component
+    {
+        public readonly List<IComponent> Components = new List<IComponent>();
 
-			component.RequestRedraw += OnRequestRedraw;
+        private Comparator<IComponent, int> compare = new Comparator<IComponent, int>(c => c.ZIndex);
 
-			OnComponentAdded (this, new ComponentEventArgs (component));
-		}
+        public int Count
+        {
+            get { return Components.Count; }
+        }
 
-		public virtual void Add(params IComponent[] components)
-		{
-			
-			Components.AddRange (components);
-			Components.Sort (compare);
+        public virtual void Add(IComponent component)
+        {
+            Components.Add(component);
+            Components.Sort(compare);
 
-			foreach (IComponent c in components)
-			{
-				c.RequestRedraw += OnRequestRedraw;
-				OnComponentAdded (this, new ComponentEventArgs (c));
-			}
+            component.RequestRedraw += OnRequestRedraw;
 
-		}
+            OnComponentAdded(this, new ComponentEventArgs(component));
+        }
 
-		public void Remove(IComponent component)
-		{
-			component.RequestRedraw -= OnRequestRedraw;
-			Components.Remove (component);
+        public virtual void Add(params IComponent[] components)
+        {
 
-			OnComponentRemoved (this, new ComponentEventArgs (component));
-		}
+            Components.AddRange(components);
+            Components.Sort(compare);
 
-		public abstract void DoLayout ();
+            foreach (IComponent c in components)
+            {
+                c.RequestRedraw += OnRequestRedraw;
+                OnComponentAdded(this, new ComponentEventArgs(c));
+            }
 
-		#region Component implementation
+        }
 
-		public override void Draw (IConsole buffer)
-		{
-			for (int x = 0; x < Width; x++) {
-				for (int y = 0; y < Height; y++) {
-					buffer.PutCharacter (x, y, ' ');
-				}
-			}
+        public void Remove(IComponent component)
+        {
+            component.RequestRedraw -= OnRequestRedraw;
+            Components.Remove(component);
 
-			// draws in order of the component's z-index
-			foreach (IComponent component in Components)
-			{
-				component.Draw (buffer);
-			}
-		}
+            OnComponentRemoved(this, new ComponentEventArgs(component));
+        }
 
-		#endregion
-	}
+        public abstract void DoLayout();
 
-	public class ComponentEventArgs : EventArgs
-	{
-		public IComponent Component {get; private set;}
+        #region Component implementation
 
-		public ComponentEventArgs(IComponent component)
-		{
-			Component = component;
-		}
-	}
+        public override void Draw(IConsole buffer)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    buffer.PutCharacter(x, y, ' ');
+                }
+            }
 
-	/// <summary>
-	/// An interface which respresents a (software) write-only console.
-	/// </summary>
-	public interface IConsole
-	{
-		int BufferWidth {get;}
-		int BufferHeight {get;}
+            // draws in order of the component's z-index
+            foreach (IComponent component in Components)
+            {
+                component.Draw(buffer);
+            }
+        }
 
-		ConsoleColor Foreground {get; set;}
-		ConsoleColor Background {get; set;}
+        #endregion
+    }
 
-		void SetCursorPosition (int x, int y);
+    public class ComponentEventArgs : EventArgs
+    {
+        public IComponent Component { get; private set; }
 
-		void PutCharacter(char c);
-		void PutCharacter(int codepoint);
+        public ComponentEventArgs(IComponent component)
+        {
+            Component = component;
+        }
+    }
 
-		void PutCharacter (int x, int y, char c);
-		void PutCharacter(int x, int y, int codepoint);
+    /// <summary>
+    /// An interface which respresents a (software) write-only console.
+    /// </summary>
+    public interface IConsole
+    {
+        int BufferWidth { get; }
+        int BufferHeight { get; }
 
-	}
+        bool SupportsUnicode { get; }
 
-	public class StandardConsole : IConsole
-	{
-		public static readonly StandardConsole INSTANCE = new StandardConsole();
+        ConsoleColor Foreground { get; set; }
+        ConsoleColor Background { get; set; }
 
-		private StandardConsole()
-		{
+        void SetCursorPosition(int x, int y);
 
-		}
+        void PutCharacter(char c);
+        void PutCharacter(int codepoint);
 
-		#region IConsole implementation
+        void PutCharacter(int x, int y, char c);
+        void PutCharacter(int x, int y, int codepoint);
 
-		public void PutCharacter (char c)
-		{
-			Console.Write (c);
-		}
+    }
 
-		public void PutCharacter (int codepoint)
-		{
-			Console.Write (char.ConvertFromUtf32 (codepoint));
-		}
+    public class StandardConsole : IConsole
+    {
+        public static readonly StandardConsole INSTANCE = new StandardConsole();
 
-		public void PutCharacter (int x, int y, char c)
-		{
-			SetCursorPosition(x, y);
-			PutCharacter (c);
-		}
+        private StandardConsole()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+        }
 
-		public void PutCharacter (int x, int y, int codepoint)
-		{
-			SetCursorPosition(x, y);
-			PutCharacter (codepoint);
-		}
+        #region IConsole implementation
 
-		public void SetCursorPosition (int x, int y)
-		{
-			Console.SetCursorPosition (x, y);
-		}
+        public void PutCharacter(char c)
+        {
+            Console.Write(c);
+        }
 
-		public int BufferWidth {
-			get {
-				return Console.BufferWidth;
-			}
-		}
+        public void PutCharacter(int codepoint)
+        {
+            Console.Write(char.ConvertFromUtf32(codepoint));
+        }
 
-		public int BufferHeight {
-			get {
-				return Console.BufferHeight;
-			}
-		}
+        public void PutCharacter(int x, int y, char c)
+        {
+            SetCursorPosition(x, y);
+            PutCharacter(c);
+        }
 
-		public ConsoleColor Foreground {
-			get {
-				return Console.ForegroundColor;
-			}
-			set {
-				Console.ForegroundColor = value;
-			}
-		}
+        public void PutCharacter(int x, int y, int codepoint)
+        {
+            SetCursorPosition(x, y);
+            PutCharacter(codepoint);
+        }
 
-		public ConsoleColor Background {
-			get {
-				return Console.BackgroundColor;
-			}
-			set {
-				Console.BackgroundColor = value;
-			}
-		}
+        public void SetCursorPosition(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+        }
 
-		#endregion
+        public bool SupportsUnicode
+        {
+            get
+            {
+                return Console.OutputEncoding == Encoding.Unicode;
+            }
+        }
+
+        public int BufferWidth
+        {
+            get
+            {
+                return Console.WindowWidth;
+            }
+        }
+
+        public int BufferHeight
+        {
+            get
+            {
+                return Console.WindowHeight;
+            }
+        }
+
+        public ConsoleColor Foreground
+        {
+            get
+            {
+                return Console.ForegroundColor;
+            }
+            set
+            {
+                Console.ForegroundColor = value;
+            }
+        }
+
+        public ConsoleColor Background
+        {
+            get
+            {
+                return Console.BackgroundColor;
+            }
+            set
+            {
+                Console.BackgroundColor = value;
+            }
+        }
+
+        #endregion
 
 
-	}
+    }
 }
 
--- SQEms/SchedulingUI/SimpleWidgets.cs
+++ SQEms/SchedulingUI/SimpleWidgets.cs
@@ -2,196 +2,222 @@ using System;
 
 namespace SchedulingUI
 {
-	public class Box : Component
-	{
-		
-		private const int TOP_LEFT = 0x250F,
-		TOP_RIGHT = 0x2513,
-		BOTTOM_LEFT = 0x2517,
-		BOTTOM_RIGHT = 0x251B,
-		VERTICAL = 0x2503,
-		HORTIZONTAL = 0x2501;
+    public class Box : Component
+    {
 
-		#region IComponent implementation
+        // Unicode Characters
+        private const int UTOP_LEFT = 0x250F,
+        UTOP_RIGHT = 0x2513,
+        UBOTTOM_LEFT = 0x2517,
+        UBOTTOM_RIGHT = 0x251B,
+        UVERTICAL = 0x2503,
+        UHORTIZONTAL = 0x2501;
 
-		public override void Draw (IConsole buffer)
-		{
-			buffer.SetCursorPosition (Left, Top);
+        private const int ACORNER = '+',
+            AVERTICAL = '|',
+            AHORIZONTAL = '-';
 
-			for (int x = Left; x < Left + Width; x++) {
-				if (x == Left) {
-					buffer.PutCharacter (TOP_LEFT);
-				} else if (x == Left + Width - 1) {
-					buffer.PutCharacter (TOP_RIGHT);
-				} else {
-					buffer.PutCharacter (HORTIZONTAL);
-				}
-			}
+        #region IComponent implementation
 
-			for (int y = Top + 1; y < Top + Height - 1; y++) {
-				buffer.PutCharacter (Left, y, VERTICAL);
-				buffer.PutCharacter (Left + Width - 1, y, VERTICAL);
-			}
+        public override void Draw(IConsole buffer)
+        {
+            
 
-			Console.SetCursorPosition (Left, Top + Height - 1);
+            buffer.SetCursorPosition(Left, Top);
 
-			for (int x = Left; x < Left + Width; x++) {
-				if (x == Left) {
-					buffer.PutCharacter (BOTTOM_LEFT);
-				} else if (x == Left + Width - 1) {
-					buffer.PutCharacter (BOTTOM_RIGHT);
-				} else {
-					buffer.PutCharacter (HORTIZONTAL);
-				}
-			}
-		}
+            for (int x = Left; x < Left + Width; x++)
+            {
+                if (x == Left)
+                {
+                    buffer.PutCharacter(UTOP_LEFT);
+                }
+                else if (x == Left + Width - 1)
+                {
+                    buffer.PutCharacter(UTOP_RIGHT);
+                }
+                else
+                {
+                    buffer.PutCharacter(UHORTIZONTAL);
+                }
+            }
 
-		#endregion
+            for (int y = Top + 1; y < Top + Height - 1; y++)
+            {
+                buffer.PutCharacter(Left, y, UVERTICAL);
+                buffer.PutCharacter(Left + Width - 1, y, UVERTICAL);
+            }
 
-		public override string ToString ()
-		{
-			return string.Format ("Box[Top={0}, Left={1}, Width={2}, Height={3}]", Top, Left, Width, Height);
-		}
+            Console.SetCursorPosition(Left, Top + Height - 1);
 
-	}
+            for (int x = Left; x < Left + Width; x++)
+            {
+                if (x == Left)
+                {
+                    buffer.PutCharacter(UBOTTOM_LEFT);
+                }
+                else if (x == Left + Width - 1)
+                {
+                    buffer.PutCharacter(UBOTTOM_RIGHT);
+                }
+                else
+                {
+                    buffer.PutCharacter(UHORTIZONTAL);
+                }
+            }
+        }
 
-	public class GridContainer : Container
-	{
-		/// <summary>
-		/// If this grid container draw and account for the borders between cells.
-		/// </summary>
-		public bool DrawBorders { get; set; }
+        #endregion
 
-		/// <summary>
-		/// The number of cells in the x direction.
-		/// </summary>
-		public int CountX { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Box[Top={0}, Left={1}, Width={2}, Height={3}]", Top, Left, Width, Height);
+        }
 
-		/// <summary>
-		/// The maximum number of cells in the y direction.
-		/// </summary>
-		public int CountY { get; set; }
+    }
 
-		public GridContainer()
-		{
-			CountX = 1;
-			CountY = 1;
-		}
+    public class GridContainer : Container
+    {
+        /// <summary>
+        /// If this grid container draw and account for the borders between cells.
+        /// </summary>
+        public bool DrawBorders { get; set; }
 
-		#region implemented abstract members of IContainer
+        /// <summary>
+        /// The number of cells in the x direction.
+        /// </summary>
+        public int CountX { get; set; }
 
-		public override void DoLayout ()
-		{
-			// calculate the rows and columns
-			int columns = Math.Min (CountX, Components.Count);
-			int rows = Math.Min(Components.Count / CountX, CountY);
+        /// <summary>
+        /// The maximum number of cells in the y direction.
+        /// </summary>
+        public int CountY { get; set; }
 
-			// find the available space for the cells
-			int available_width = Width - (DrawBorders ? columns + 1 : 0);
-			int available_height = Height - (DrawBorders ? rows + 1 : 0);
+        public GridContainer()
+        {
+            CountX = 1;
+            CountY = 1;
+        }
 
-			int offset = DrawBorders ? 1 : 0;
+        #region implemented abstract members of IContainer
 
-			double width = available_width / columns + offset;
-			double height = available_height / rows + offset;
+        public override void DoLayout()
+        {
+            // calculate the rows and columns
+            int columns = Math.Min(CountX, Components.Count);
+            int rows = Math.Min(Components.Count / CountX, CountY);
 
-			for (int x = 0; x < columns; x++) {
-				for (int y = 0; y < rows; y++) {
-					IComponent component = Components[y * columns + x];
-					
-					component.Top = Top + (int)(height * y) + offset;
-					component.Left = Left + (int)(width * x) + offset;
+            // find the available space for the cells
+            int available_width = Width - (DrawBorders ? columns + 1 : 0);
+            int available_height = Height - (DrawBorders ? rows + 1 : 0);
 
-					component.Width = (int)(width * x % 1 + width) - offset;
-					component.Height = (int)(height * y % 1 + height) - offset;
-					
-					if(component is Container)
-					{
-						(component as Container).DoLayout ();
-					}
-				}
-			}
+            int offset = DrawBorders ? 1 : 0;
 
-		}
+            double width = available_width / columns + offset;
+            double height = available_height / rows + offset;
 
-		#endregion
+            for (int x = 0; x < columns; x++)
+            {
+                for (int y = 0; y < rows; y++)
+                {
+                    IComponent component = Components[y * columns + x];
 
-	}
+                    component.Top = Top + (int)(height * y) + offset;
+                    component.Left = Left + (int)(width * x) + offset;
 
-	public class FlowContainer : GridContainer
-	{
-		public bool Vertical { get; set; }
+                    component.Width = (int)(width * x % 1 + width) - offset;
+                    component.Height = (int)(height * y % 1 + height) - offset;
 
-		public FlowContainer():
-			base()
-		{
-			ComponentAdded += (object sender, ComponentEventArgs e) => UpdateCount ();
-			ComponentRemoved += (object sender, ComponentEventArgs e) => UpdateCount ();
-		}
+                    if (component is Container)
+                    {
+                        (component as Container).DoLayout();
+                    }
+                }
+            }
 
-		private void UpdateCount()
-		{
-			if (Vertical) {
-				CountX = 1;
-				CountY = Components.Count;
-			} else {
-				CountX = Components.Count;
-				CountY = 1;
-			}
+        }
 
-			DoLayout ();
-		}
+        #endregion
 
-	}
+    }
 
-	public class RootContainer : Container
-	{
-		public IConsole Console { get; private set; }
+    public class FlowContainer : GridContainer
+    {
+        public bool Vertical { get; set; }
 
-		public RootContainer(IConsole console)
-		{
-			ComponentAdded += CheckCount;
-			ComponentRemoved += CheckCount;
+        public FlowContainer() :
+            base()
+        {
+            ComponentAdded += (object sender, ComponentEventArgs e) => UpdateCount();
+            ComponentRemoved += (object sender, ComponentEventArgs e) => UpdateCount();
+        }
 
-			Console = console;
-		}
+        private void UpdateCount()
+        {
+            if (Vertical)
+            {
+                CountX = 1;
+                CountY = Components.Count;
+            }
+            else
+            {
+                CountX = Components.Count;
+                CountY = 1;
+            }
 
-		private void CheckCount(object sender, ComponentEventArgs e)
-		{
-			if (Components.Count > 1) {
-				System.Diagnostics.Debug.WriteLine ("Warning: RootContainer has more than one child");
-			}
-		}
+            DoLayout();
+        }
 
-		public void Draw()
-		{
-			Draw (Console);
-		}
+    }
 
-		#region implemented abstract members of IContainer
+    public class RootContainer : Container
+    {
+        public IConsole Console { get; private set; }
 
-		public override void DoLayout ()
-		{
-			if (Components.Count > 0)
-			{
-				IComponent root = Components [0];
+        public RootContainer(IConsole console)
+        {
+            ComponentAdded += CheckCount;
+            ComponentRemoved += CheckCount;
 
-				root.Top = 0;
-				root.Left = 0;
-				root.Height = Console.BufferHeight;
-				root.Width = Console.BufferWidth;
+            Console = console;
+        }
 
-				if(root is Container)
-				{
-					(root as Container).DoLayout ();
-				}
-			}
-		}
+        private void CheckCount(object sender, ComponentEventArgs e)
+        {
+            if (Components.Count > 1)
+            {
+                System.Diagnostics.Debug.WriteLine("Warning: RootContainer has more than one child");
+            }
+        }
 
-		#endregion
+        public void Draw()
+        {
+            Draw(Console);
+        }
+
+        #region implemented abstract members of IContainer
+
+        public override void DoLayout()
+        {
+            if (Components.Count > 0)
+            {
+                IComponent root = Components[0];
+
+                root.Top = 0;
+                root.Left = 0;
+                root.Height = Console.BufferHeight;
+                root.Width = Console.BufferWidth;
+
+                if (root is Container)
+                {
+                    (root as Container).DoLayout();
+                }
+            }
+        }
+
+        #endregion
 
 
-	}
+    }
 
 }
 
