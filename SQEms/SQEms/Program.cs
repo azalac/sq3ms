697e8051a783388bdc1538265e762b91e49ae4e7
azalac <zalac.austin@gmail.com>
2018-11-05 11:43:20 -0500

    Changed test main to database classes

--- SQEms/SQEms/Program.cs
+++ SQEms/SQEms/Program.cs
@@ -1,10 +1,12 @@
 ﻿using SchedulingUI;
+using Support;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Configuration;
 
 namespace SQEms
 {
@@ -12,70 +14,11 @@ namespace SQEms
     {
         static void Main(string[] args)
 		{
-			RootContainer root = new RootContainer (StandardConsole.INSTANCE);
+			DatabaseManager db_manager = new DatabaseManager ();
 
-			TabbedPane pane = new TabbedPane ();
+			db_manager.LoadAll ();
 
-			InputArea inputs = new InputArea ("one", "two", "three")
-			{
-				RowHeight = 2,
-                InputWidth = 20
-			};
-
-			Button s = new Button ()
-			{
-				Text = "Submit"
-			};
-			
-			Button s2 = new Button ()
-			{
-				Text = "Submit"
-			};
-			
-			GridContainer flow = new GridContainer ()
-			{
-				CountY = 2
-			};
-
-			s.Action += (object sender, EventArgs e) => {
-				pane.SelectedIndex = 1;
-				pane.DoLayout();
-				pane.OnRequestRedraw(s, null);
-				System.Diagnostics.Debug.Write("One");
-			};
-
-			s2.Action += (object sender, EventArgs e) => {
-				pane.SelectedIndex = 0;
-				pane.DoLayout();
-				pane.OnRequestRedraw(s2, null);
-				System.Diagnostics.Debug.Write("Two");
-			};
-
-			inputs ["three"] = s;
-
-			flow.Add (new Label () { Text = "Hello World"}, s2);
-
-			pane.Add (inputs, flow);
-			root.Add (pane);
-
-			inputs.DoLayout ();
-			root.DoLayout ();
-			root.Draw ();
-
-			KeyboardInput input = new KeyboardInput (root)
-			{
-				ExitKey = ConsoleKey.Escape
-			};
-
-			Console.CursorVisible = false;
-
-			input.StartThread ();
-
-			input.InternalThread.Join ();
-
-			Console.Clear ();
-			Console.ResetColor ();
-			Console.CursorVisible = true;
+			Console.ReadKey ();
 		}
     }
 }
