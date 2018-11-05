5016d094b8be0ef903821d39b4068ab2ad3cca32
azalac <zalac.austin@gmail.com>
2018-11-05 12:43:13 -0500

    Updated UI Testing

--- SQEms/SchedulingUI/Framework/Testing.cs
+++ SQEms/SchedulingUI/Framework/Testing.cs
@@ -6,74 +6,74 @@ using System.Threading.Tasks;
 
 namespace SchedulingUI
 {
-    public static class UITesting
-    {
-		public const int TOP_LEFT = 0x250F,
-		TOP_RIGHT = 0x2513,
-		BOTTOM_LEFT = 0x2517,
-		BOTTOM_RIGHT = 0x251B,
-		VERTICAL = 0x2503,
-		HORTIZONTAL = 0x2501,
-		EMPTY = -1;
-
-		public const int UP = 0x1, DOWN = 0x2, LEFT = 0x4, RIGHT = 0x8;
-
-        public static void test_box()
-        {
-			draw_box (0, 0, Console.BufferWidth, Console.BufferHeight);
-        }
-
-		public static void draw_box(int left, int top, int width, int height)
+	public static class Testing
+	{
+		public static void TestInterface()
 		{
-			for (int x = left; x < left + width; x++) {
-				if (x == left) {
-					Console.Write (char.ConvertFromUtf32(TOP_LEFT));
-				} else if (x == left + width - 1) {
-					Console.Write (char.ConvertFromUtf32(TOP_RIGHT));
-				} else {
-					Console.Write (char.ConvertFromUtf32(HORTIZONTAL));
-				}
-			}
+			RootContainer root = new RootContainer (StandardConsole.INSTANCE);
 
-			for (int y = top + 1; y < top + height - 1; y++) {
-				Console.SetCursorPosition (left, y);
-				Console.Write (char.ConvertFromUtf32 (VERTICAL));
-				Console.SetCursorPosition (left + width - 1, y);
-				Console.Write (char.ConvertFromUtf32 (VERTICAL));
-			}
+			TabbedPane pane = new TabbedPane ();
 
-			Console.SetCursorPosition (left, top + height - 1);
+			InputArea inputs = new InputArea ("one", "two", "three")
+			{
+				RowHeight = 2,
+				InputWidth = 20
+			};
 
-			for (int x = left; x < left + width; x++) {
-				if (x == left) {
-					Console.Write (char.ConvertFromUtf32(BOTTOM_LEFT));
-				} else if (x == left + width - 1) {
-					Console.Write (char.ConvertFromUtf32(BOTTOM_RIGHT));
-				} else {
-					Console.Write (char.ConvertFromUtf32(HORTIZONTAL));
-				}
-			}
+			Button s = new Button ()
+			{
+				Text = "Submit"
+			};
+
+			Button s2 = new Button ()
+			{
+				Text = "Submit"
+			};
+
+			GridContainer flow = new GridContainer ()
+			{
+				CountY = 2
+			};
+
+			s.Action += (object sender, EventArgs e) => {
+				pane.SelectedIndex = 1;
+				pane.OnRequestRedraw(s, null);
+				//s.OnRequestFocus(s, new ComponentEventArgs(s2));
+				System.Diagnostics.Debug.Write("One");
+			};
+
+			s2.Action += (object sender, EventArgs e) => {
+				pane.SelectedIndex = 0;
+				pane.OnRequestRedraw(s2, null);
+				//s2.OnRequestFocus(s2, new ComponentEventArgs(s));
+				System.Diagnostics.Debug.Write("Two");
+			};
+
+			inputs ["three"] = s;
+
+			flow.Add (new Label () { Text = "Hello World"}, s2);
+
+			pane.Add (inputs, flow);
+			root.Add (flow);
+
+			root.DoLayout ();
+			root.Draw ();
+
+			KeyboardInput input = new KeyboardInput (root)
+			{
+				ExitKey = ConsoleKey.Escape
+			};
+
+			Console.CursorVisible = false;
+
+			input.StartThread ();
+
+			input.InternalThread.Join ();
+
+			Console.Clear ();
+			Console.ResetColor ();
+			Console.CursorVisible = true;
 		}
-
-		public static int char_for_code(int code)
-		{
-			switch (code) {
-			case UP | RIGHT:
-				return BOTTOM_LEFT;
-			case UP | LEFT:
-				return BOTTOM_RIGHT;
-			case RIGHT | DOWN:
-				return TOP_LEFT;
-			case LEFT | DOWN:
-				return TOP_RIGHT;
-			case UP | DOWN:
-				return VERTICAL;
-			case LEFT | RIGHT:
-				return HORTIZONTAL;
-			default:
-				return EMPTY;
-			}
-		}
-
-    }
+	}
+	
 }
