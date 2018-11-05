a0e07a245b524214244e9ac850d03b4f707ae1b9
azalac <zalac.austin@gmail.com>
2018-11-05 13:39:33 -0500

    Added skeleton classes for billing codes

---
+++ SQEms/Billing/BillingCodeEntry.cs
@@ -0,0 +1,64 @@
+﻿using Demographics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Billing
+{
+    public struct BillingCodeEntry
+    {
+        public string DateOfService;
+
+        public string HCN;
+
+        public SexTypes Sex;
+
+        public string BillingCode;
+
+        public string Fee;
+
+        public BillingCodeResponse Response;
+
+        /// <summary>
+        /// Parses a BillingCodeEntry from a string.
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// The first 8 characters are the DateOfService (YYYYMMDD).
+        /// The next 12 characters are the patient's HCN (10 numeric, 2 alphabetic).
+        /// The next 1 character is the patient's sex (M/F/I/H). <see cref="SexTypes"/>
+        /// The next 11 characters are the Fee (7 integer, 4 decimal, no period, zero padded).
+        /// 
+        /// </remarks>
+        /// <param name="str">The string</param>
+        /// <returns>The BillingCodeEntry</returns>
+        public static BillingCodeEntry ParseFromString(string str)
+        {
+            //TODO this
+            return default(BillingCodeEntry);
+        }
+
+        /// <summary>
+        /// Converts this Billing Code Entry to a string. <see cref="BillingCodeEntry.ParseFromString(string)"/>
+        /// </summary>
+        /// <returns>This, as a string</returns>
+        public override string ToString()
+        {
+            //TODO this
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// All valid billing code responses.
+    /// </summary>
+    public enum BillingCodeResponse
+    {
+        NONE,
+        PAID,
+        DECL,
+        FHCV,
+        CMOH
+    }
+}
---
+++ SQEms/Billing/BillingMasterEntry.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Billing
+{
+    public class BillingMasterEntry
+    {
+        public string FeeCode { get; private set; }
+
+        public string EffectiveDate { get; private set; }
+
+        public string DollarAmount { get; private set; }
+        
+        /// <summary>
+        /// Parses a string into a Billing Entry.
+        /// </summary>
+        /// <remarks>
+        /// 
+        /// The first 4 characters are the fee code.
+        /// The next 8 characters are the effective date (YYYYMMDD).
+        /// The last 11 characters are the dollar amount (7.2 as float format).
+        /// 
+        /// </remarks>
+        /// <param name="str">The string</param>
+        /// <returns>The parsed BillingMasterEntry</returns>
+        public static BillingMasterEntry ParseFromString(string str)
+        {
+            //TODO this
+            return null;
+        }
+
+        /// <summary>
+        /// Converts this entry to a string. <see cref="BillingMasterEntry.ParseFromString(string)"/>
+        /// </summary>
+        /// <returns>This, as a string</returns>
+        public override string ToString()
+        {
+            //TODO this
+            return null;
+        }
+        
+    }
+}
