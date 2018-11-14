e077ce6076115e193f08a311c23b94c4d923f27e
azalac <zalac.austin@gmail.com>
2018-11-14 14:48:22 -0500

    Started patient booking controller

---
+++ SQEms/SchedulingUI/SchedulingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingUI
+{
+    /// <summary>
+    /// Handles the interface for scheduling a patient.
+    /// </summary>
+    /// <remarks>
+    /// Workflow:
+    /// 
+    /// Select a timeslot
+    /// 
+    /// Find the patient & caregiver
+    /// 
+    /// </remarks>
+    public class SchedulingController
+    {
+    }
+
+    public class TimeSlotSelectionController : GridContainer
+    {
+        private readonly InputController controller = new InputController();
+
+
+    }
+}
