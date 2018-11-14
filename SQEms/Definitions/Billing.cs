235d9965a4a041b591957d999cff403e0e503fb2
azalac <zalac.austin@gmail.com>
2018-11-14 14:54:56 -0500

    Added MOH billing status codes enum

---
+++ SQEms/Definitions/Billing.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Definitions
+{
+	/// <summary>
+	/// All valid billing code responses.
+	/// </summary>
+	public enum BillingCodeResponse
+	{
+		NONE,
+		PAID,
+		DECL,
+		FHCV,
+		CMOH
+	}
+}
+
