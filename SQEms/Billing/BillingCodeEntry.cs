42c3c675af3928f1b703559cb1e0cdd59296b642
azalac <zalac.austin@gmail.com>
2018-11-16 16:07:58 -0500

    Updated billing with comments

--- SQEms/Billing/BillingCodeEntry.cs
+++ SQEms/Billing/BillingCodeEntry.cs
@@ -4,51 +4,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Definitions;
+using Support;
 
 namespace Billing
 {
-    public struct BillingCodeEntry
+    /// <summary>
+    /// Reads & Writes MOH billing files and generates monthly summaries.
+    /// </summary>
+    /// <remarks>
+    /// 
+    /// Entry format:
+    /// 
+    /// The first 8 characters are the DateOfService (YYYYMMDD).
+    /// The next 12 characters are the patient's HCN (10 numeric, 2 alphabetic).
+    /// The next 1 character is the patient's sex (M/F/I/H, <see cref="SexTypes"/>).
+    /// The next 11 characters are the Fee (7 integers, 4 decimals, no period, zero padded).
+    /// 
+    /// </remarks>
+    public class BillingFileInteraction
     {
-        public string DateOfService;
 
-        public string HCN;
+        private readonly DatabaseTable billing;
+        
+        public BillingFileInteraction(DatabaseManager database)
+        {
+            billing = database["Billing"];
+        }
+        
+        /// <summary>
+        /// Parses the file at the given path into the database.
+        /// </summary>
+        /// <param name="path"></param>
+        public void Parse(string path)
+        {
 
-        public SexTypes Sex;
 
-        public string BillingCode;
+        }
+        
+        /// <summary>
+        /// Writes the billing information to the file at the given path.
+        /// </summary>
+        /// <param name="path"></param>
+        public void Write(string path)
+        {
 
-        public string Fee;
-
-        public BillingCodeResponse Response;
+        }
 
         /// <summary>
-        /// Parses a BillingCodeEntry from a string.
+        /// Generates a monthly billing summary file.
         /// </summary>
+        /// <param name="path">The path to output to</param>
         /// <remarks>
+        /// Fields:
         /// 
-        /// The first 8 characters are the DateOfService (YYYYMMDD).
-        /// The next 12 characters are the patient's HCN (10 numeric, 2 alphabetic).
-        /// The next 1 character is the patient's sex (M/F/I/H). <see cref="SexTypes"/>
-        /// The next 11 characters are the Fee (7 integer, 4 decimal, no period, zero padded).
+        /// Total Encounters Billed (count)
+        /// Total Billed Procedures (dollars)
+        /// Received Total (dollars)
+        /// Received Percentage (RT/TBP * 100%)
+        /// Average Billing (RT / TEB in dollars)
+        /// Num. Encounters to Follow-up (count of FHCV and CMOH)
         /// 
+        /// Format is supposed to be CSV, but no layout specified?
         /// </remarks>
-        /// <param name="str">The string</param>
-        /// <returns>The BillingCodeEntry</returns>
-        public static BillingCodeEntry ParseFromString(string str)
+        public void WriteStatistics(string path)
         {
-            //TODO this
-            return default(BillingCodeEntry);
+
         }
 
-        /// <summary>
-        /// Converts this Billing Code Entry to a string. <see cref="BillingCodeEntry.ParseFromString(string)"/>
-        /// </summary>
-        /// <returns>This, as a string</returns>
-        public override string ToString()
-        {
-            //TODO this
-            return null;
-        }
     }
-
+    
 }
--- SQEms/Billing/BillingMasterEntry.cs
+++ SQEms/Billing/BillingMasterEntry.cs
@@ -1,18 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Support;
+using System;
 
 namespace Billing
 {
+    /// <summary>
+    /// Represents the master entry for a billing code description.
+    /// </summary>
     public class BillingMasterEntry
     {
+        /// <summary>
+        /// The fee code for this billing description.
+        /// </summary>
         public string FeeCode { get; private set; }
 
+        /// <summary>
+        /// The effective starting date for the billing description.
+        /// </summary>
         public string EffectiveDate { get; private set; }
 
+        /// <summary>
+        /// The amount the procedure costs.
+        /// </summary>
         public string DollarAmount { get; private set; }
-        
+
+        /// <summary>
+        /// Reads the master description file into the database.
+        /// </summary>
+        /// <param name="path">The master file path</param>
+        /// <param name="database">The database to insert into</param>
+        /// <remarks>
+        /// 
+        /// Calls <see cref="ParseFromString(string)"/>.
+        /// 
+        /// Pass each line to the method. If the method throws an ArgumentException, 
+        /// log the error and ignore the line.
+        /// 
+        /// </remarks>
+        public static void Initialize(string path, DatabaseManager database)
+        {
+            // this table is not yet implemented
+            DatabaseTable BillingDescription = database["BillingMaster"];
+        }
+
         /// <summary>
         /// Parses a string into a Billing Entry.
         /// </summary>
@@ -25,17 +54,8 @@ namespace Billing
         /// </remarks>
         /// <param name="str">The string</param>
         /// <returns>The parsed BillingMasterEntry</returns>
+        /// <exception cref="ArgumentException">If the entry is invalid.</exception>
         public static BillingMasterEntry ParseFromString(string str)
-        {
-            //TODO this
-            return null;
-        }
-
-        /// <summary>
-        /// Converts this entry to a string. <see cref="BillingMasterEntry.ParseFromString(string)"/>
-        /// </summary>
-        /// <returns>This, as a string</returns>
-        public override string ToString()
         {
             //TODO this
             return null;
