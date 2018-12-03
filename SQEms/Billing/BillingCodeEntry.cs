33470c57509d312cda90593325b49e50ade502eb
bribble3982 <bribble3982@conestogac.on.ca>
2018-12-03 13:41:32 -0500

    Code entry started

--- SQEms/Billing/BillingCodeEntry.cs
+++ SQEms/Billing/BillingCodeEntry.cs
@@ -1,10 +1,18 @@
-﻿using Demographics;
+﻿/*
+* FILE          : BillingCodeEntry.cs
+* PROJECT       : INFO-2180 Software Quality 1, Term Project
+* PROGRAMMER    : Blake Ribble and Austin Zalac
+* FIRST VERSION : November 1, 2018
+*/
+
+using Demographics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Definitions;
 using Support;
+using System.IO;    
 
 namespace Billing
 {
@@ -23,32 +31,93 @@ namespace Billing
     /// </remarks>
     public class BillingFileInteraction
     {
+        //Definitions of class level variables
+        private static DatabaseTable billing;
+        private static DatabaseTable billingMaster;
 
-        private readonly DatabaseTable billing;
+        /// <summary>
+        /// Constructor that initializes class level variables
+        /// </summary>
+        /// <param name="database"> Used to obtain the information from database</param>
         
         public BillingFileInteraction(DatabaseManager database)
         {
             billing = database["Billing"];
+            billingMaster = database["BillingMaster"];        
         }
-        
-        /// <summary>
-        /// Parses the file at the given path into the database.
-        /// </summary>
-        /// <param name="path"></param>
-        public void Parse(string path)
-        {
 
-
-        }
-        
         /// <summary>
         /// Writes the billing information to the file at the given path.
         /// </summary>
-        /// <param name="path"></param>
-        public void Write(string path)
+        /// <param name="database"> Used to obtain the information for billing code</param>
+        /// <param name="path"> Path of the billing code</param>
+
+        public static void CreateBillingCode(DatabaseManager database, string path, string feeCode, object appID)
         {
+            int ID = (int)appID;
+
+            //Gets the current date of service
+            string date = GetDateOfService();
+
+            //Create a new instance of the log class - used for errors
+            Logging logger = new Logging();
+            
+            //Set database table variables with information 
+            DatabaseTable Master = database["BillingMaster"];
+            DatabaseTable testBilling = database["Billing"];
+
+            //Set an object variable that holds the primary key for dollar amount
+            object name = null;
+
+            try
+            {
+                //Look in the Masterfile database for the feeCode being searched for
+                name = Master.WhereEquals<string>("BillingCode", feeCode).First();
+            }
+
+            //If the feeCode doesn't exist
+            catch(InvalidOperationException)
+            {
+                logger.Log(LoggingInfo.ErrorLevel.ERROR, "Billing Code was not found in database");
+                return;
+            }
+            
+            //Convert the name of the object to a string
+            string BillName = name.ToString();
+
+            //Use the feeCode found and search for the fee amount in the DollarAmount coloumn
+            object cost = Master[BillName, "DollarAmount"];
+
+            //Convert the cost of the object to a string
+            string costWithDecimal = cost.ToString();
+
+            string[] numSplit = costWithDecimal.Split('.');
+
+            string finalCost = numSplit[0] + numSplit[1];
+
+
+            //Create an instance of the patients table to obtain health card with patient ID
+            DatabaseTable patientInfo = database["Patients"];
+
+            //Store the info in an object
+            object info = patientInfo[ID, "HCN"];
+
+            //Convert object to string
+            string patientHCN = info.ToString();
+
+            //Create the billing code to send to text file - need to left pad final code and fee code
+            string billingFile = date + patientHCN + feeCode + finalCost + "00";
+
+            //Get the length of the billing file
+            int length = billingFile.Length;
+
+            //Length is 36 characters long for file, subtract what we have, the remaining number is how many zeroes needed for left padding
+
+            Console.WriteLine(billingFile);
 
         }
+
+
 
         /// <summary>
         /// Generates a monthly billing summary file.
@@ -69,6 +138,13 @@ namespace Billing
         public void WriteStatistics(string path)
         {
 
+        }
+
+        public static string GetDateOfService()
+        {
+            DateTime theDate = DateTime.Now;
+            string strToday = theDate.ToString("yyyyMMdd");
+            return strToday;
         }
 
     }
