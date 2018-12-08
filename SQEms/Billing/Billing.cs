2eb7196b8a0d46affdfb7579e572d6242c56b7ab
billyParmenter <bparmenter6387@conestogac.on.ca>
2018-12-07 19:49:08 -0500

    *Austin* Look at line 83 finished methods

--- SQEms/Billing/Billing.cs
+++ SQEms/Billing/Billing.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Support;
+using System.Linq;
 
 namespace Billing
 {
@@ -8,7 +9,7 @@ namespace Billing
     /// Handles billing code database interaction.
     /// </summary>
 	public class Billing
-	{
+    {
 
         /*
          * I'm not sure about the proper way to go about billing handling, so I
@@ -22,11 +23,19 @@ namespace Billing
          */
 
         private readonly DatabaseTable BillingEntries;
+        private readonly DatabaseTable BillingMaster;
+        private readonly DatabaseTable Appointments;
+        private readonly DatabaseTable Patients;
 
-		public Billing (DatabaseManager database)
-		{
-			BillingEntries = database ["Billing"];
-		}
+
+        public Billing(DatabaseManager database)
+        {
+            BillingEntries = database["Billing"];
+            BillingMaster = database["BillingMaster"];
+            Appointments = database["Appointments"];
+            Patients = database["Patients"];
+
+        }
 
         /// <summary>
         /// Adds a billing code to the given appointment
@@ -34,18 +43,71 @@ namespace Billing
         /// <param name="AppointmentID">The appointment</param>
         /// <param name="code">The billing code</param>
 		public void AddBillingCode(int AppointmentID, string code)
-		{
+        {
+            //Get billingID
+            int billingID = BillingEntries.GetMaximum("BillingID") + 1;
 
-		}
+            //Get Key of Appointments row
+            object key = Appointments.WhereEquals("AppointmentID", AppointmentID).First();
+
+            int.TryParse(key.ToString(), out int AppPK);
+
+            //Get month week day from appointment
+            int.TryParse(Appointments[AppPK, "Month"].ToString(), out int month);
+            int.TryParse(Appointments[AppPK, "Week"].ToString(), out int week);
+            int.TryParse(Appointments[AppPK, "Day"].ToString(), out int day);
+            string date = (month + ", " + week + ", " + day);
+
+            //Get key of Patients where PatientID = PatientID in Appointments
+            key = Patients.WhereEquals("PatientID", Appointments[AppPK, "PatientID"]).First();
+
+            int.TryParse(key.ToString(), out int Ppk);
+
+            //Get HCN and sex
+            string HCN = Patients[Ppk, "HCN"].ToString();
+            Definitions.SexTypes sex = (Definitions.SexTypes)Patients[Ppk, "sex"];
+
+            //Get key of BillingMaster where BillingCode = code
+            key = BillingMaster.WhereEquals("BillingCode", code).First();
+
+            //Get fee from BillinMaster
+            string fee = BillingMaster[key, "DollarAmount"].ToString();
+
+
+
+
+
+
+
+
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NULL breaks code What should be here??????????????????????????????????????
+
+
+
+
+
+
+
+
+            object codeResponse = Definitions.BillingCodeResponse.PAID;
+
+            BillingEntries.Insert(billingID, AppointmentID, date, HCN, sex, code, fee, codeResponse);
+        }
 
         /// <summary>
-        /// Removes a billing code for a given appointment
+        /// Removes Billing code
         /// </summary>
         /// <param name="AppointmentID">The appointment</param>
         /// <param name="code">The billing code</param>
         public void RemoveBillingCode(int AppointmentID, string code)
         {
-
+            foreach (object key in BillingEntries.WhereEquals("AppointmentID", AppointmentID))
+            {
+                if (BillingEntries[key, "BillingCode"].ToString() == code)
+                {
+                    BillingEntries.DeleteRow(key);
+                }
+            }
         }
 
         /// <summary>
@@ -55,10 +117,14 @@ namespace Billing
         /// <returns>All billing entries for the appointment</returns>
         public IEnumerable<int> FindBillingEntries(int AppointmentID)
         {
-            // if you want, you can implement this by using 'yield' in order to speed up searching
-            // see https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/yield
 
-            return null;
+            foreach (object key in BillingEntries.WhereEquals("AppointmentID", AppointmentID))
+            {
+                int.TryParse(key.ToString(), out int pk);
+                yield return pk;
+            }
+
+            yield break;
         }
     }
 }
