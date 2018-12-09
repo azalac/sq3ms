7f59f0ee72b5d4745b4feb35131af6d64631374d
azalac <zalac.austin@gmail.com>
2018-12-09 02:37:11 -0500

    Added helper logging class which isolates different types of log messages

---
+++ SQEms/SchedulingUI/DebugLog.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulingUI
+{
+    class DebugLog
+    {
+        public const int COMPONENT_EVENTS = 0b1,
+            CONTROLLER_EVENTS = 0b10,
+            ETC_EVENTS = 0b100;
+
+        private static int Level = CONTROLLER_EVENTS | ETC_EVENTS;
+
+        public static void Log(int level, object message)
+        {
+            if((level & Level) != 0)
+            {
+                System.Diagnostics.Debug.WriteLine(message);
+            }
+        }
+
+        public static void LogComponent(object message)
+        {
+            Log(COMPONENT_EVENTS, message);
+        }
+
+        public static void LogController(object message)
+        {
+            Log(CONTROLLER_EVENTS, message);
+        }
+
+        public static void LogOther(object message)
+        {
+            Log(ETC_EVENTS, message);
+        }
+
+    }
+}
