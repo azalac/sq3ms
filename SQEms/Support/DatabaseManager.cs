4e0e768900f8bddfc99c840eedfcecd723a71710
azalac <zalac.austin@gmail.com>
2018-12-10 02:43:53 -0500

    Renamed db files and fixed bug when db files didn't exist

--- SQEms/Support/DatabaseManager.cs
+++ SQEms/Support/DatabaseManager.cs
@@ -23,8 +23,8 @@ namespace Support
 		static DatabaseManager()
 		{
 			AddTable(new TestTable(), "./test_table.dat");
-            AddTable(new BillingCodeTable(), "./billingCode_table.dat");
-            AddTable(new BillingMasterTable(), "./billingMaster_table.dat");
+            AddTable(new BillingCodeTable(), "./billable_procedures.dat");
+            AddTable(new BillingMasterTable(), "./billing_master.dat");
             AddTable(new PeopleTable (), "./people.dat");
 			AddTable(new AppointmentTable (), "./appointments.dat");
 			AddTable(new HouseholdTable (), "./households.dat");
@@ -117,11 +117,6 @@ namespace Support
 		/// </summary>
 		public void Load()
 		{
-            if(!File.Exists(Location))
-            {
-                File.Create(Location);
-            }
-
             // call the custom reader if there is one
             if(prototype.CustomReader != null)
             {
@@ -129,7 +124,9 @@ namespace Support
                 return;
             }
 
-            using (BinaryReader input = new BinaryReader(new FileStream(Location, FileMode.OpenOrCreate)))
+            FileStream fstream = new FileStream(Location, FileMode.OpenOrCreate, FileAccess.Read, FileShare.ReadWrite);
+
+            using (BinaryReader input = new BinaryReader(fstream))
             {
                 while (input.PeekChar() != -1)
                 {
@@ -138,6 +135,8 @@ namespace Support
                     Data[row[prototype.PrimaryKeyIndex]] = row;
                 }
             }
+
+            fstream.Close();
 		}
 
 		/// <summary>
@@ -168,13 +167,17 @@ namespace Support
                     return;
                 }
 
-                using (BinaryWriter output = new BinaryWriter(new FileStream(Location, FileMode.Create)))
+                FileStream fstream = new FileStream(Location, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+
+                using (BinaryWriter output = new BinaryWriter(fstream))
                 {
                     foreach (object[] row in Data.Values)
                     {
                         prototype.SaveRowImpl(output, row);
                     }
                 }
+
+                fstream.Close();
 			}
 		}
 
